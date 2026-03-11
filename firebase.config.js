2ebb71c0f07f0100fe333cc59dc3365d00a7afe9
Nitai Charan <nitaicharan@gmail.com>
2026-03-11 11:42:52 +0000

    34. Firebase SDK Setup

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// Firebase SDK Configuration
+export const firebaseConfig = {
+  projectId: "pocket-heist-20260311",
+  appId: "1:717773884558:web:4f5196319db0af7bdc0ddf",
+  storageBucket: "pocket-heist-20260311.firebasestorage.app",
+  apiKey: "AIzaSyDEGRITsr3yNzkVwjeBWSE8EoCnyH3Bop8",
+  authDomain: "pocket-heist-20260311.firebaseapp.com",
+  messagingSenderId: "717773884558",
+  databaseURL: "https://pocket-heist-20260311-default-rtdb.firebaseio.com",
+};
